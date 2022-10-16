27c2ec5f07f1bdb83c92c52985625b8fe572dae9
Been0701 <dlwptlwkr93@naver.com>
2022-10-16 23:11:57 +0900

    배경화면 설정, 날씨 추가

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = '8d8bc42c59547cc73b31b6649a9a6bce';
+
 function onGeoOk(location){
     const lat = location.coords.latitude;
     const long = location.coords.longitude;
     console.log("You live in ",lat, long);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
+    fetch(url) // Javascript로 브라우저에 url을 호출함
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}도`;
+        city.innerText = data.name;
+    });
 }
 function onGeoError(){
     alert("위치를 찾을 수 없어 날씨 정보 제공이 어렵습니다.")
